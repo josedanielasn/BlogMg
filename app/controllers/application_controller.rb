e72ff70a288d6ca010554c08baf882fb1e30be4c
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-20 18:07:02 +0800

    Adds helper for finding user and blog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def find_blog
+    @user = User.find(params[:user_id])
+    !@user.blogs.empty? ? @blog = @user.blogs.find(params[:blog_id]) : 'nil'
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
