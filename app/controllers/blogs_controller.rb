0b182b85b42f0fa3e94c2b3bff9f7dbc82626f03
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-20 18:08:10 +0800

    Adds CRUD for blog

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,48 @@
+class BlogsController < ApplicationController
+
+  def new
+    find_blog
+    @blog = @user.blogs.build()
+  end
+
+  def create
+    find_blog
+    @blog = @user.blogs.build(blog_params)
+    if @blog.save 
+      redirect_to show_user_path(@user)
+    else
+      render 'users/show'
+    end
+  end
+
+  def show
+    find_blog
+    @comments = @blog.comments
+  end
+
+  def edit
+    find_blog
+  end
+
+  def update
+    find_blog
+    if @blog.update(blog_params)
+      redirect_to show_blog_path(@blog)
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    find_blog
+    @blog.destroy
+    redirect_to show_user_path(@user)
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content_text)
+  end
+
+end
