8388afd5ebd2f8a7229b05298b690383b890b157
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-20 18:05:42 +0800

    Adds data association to blog/ Adds validation the following columns: firstname, lastname, username, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :blogs
+  validates :firstname, presence: true, length: {minimum:2, maximum:15}
+  validates :lastname, presence: true, length: {minimum:2, maximum:15}
+  validates :username, presence: true, length: {minimum:2, maximum:15}, uniqueness: true
+  validates :password, presence: true, length: {minimum:6}
 end
