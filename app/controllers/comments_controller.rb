042a31e773147f1a55d1e151675013c2570ba72f
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-20 18:08:35 +0800

    Adds create and delete method for comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+  def create
+    find_blog
+    @comment = @blog.comments.create(comment_params)
+    
+    redirect_to show_blog_path(user_id: @user.id, blog_id: @blog.id)
+  end
+
+  def delete
+    @comment = Comment.find(params[:comment_id])   
+    @comment.destroy
+    redirect_to blog_show_path(user_id: @blog.user.id, blog_id: @blog.id)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :comment_text)
+  end
+end
