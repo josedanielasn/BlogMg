1969cfedc8f6ede20968af927db934552a3424f5
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-20 18:07:27 +0800

    Adds CRUD method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :delete]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @blogs = @user.blogs
+  end
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to index_user_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
+  def udpate
+    if @user.update(user_params)
+      redirect_to index_user_path
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @user.destroy
+    redirect_to index_user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :firstname, :lastname ,:password)
+  end
+
+  
 end
