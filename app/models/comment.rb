98b00875a6c6f5628f05dd7cb0c88d6e9371c27f
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-20 18:04:32 +0800

    Adds data association to blog/ Adds validation for the following columns: commenter, comment_text

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :blog
+  validates :commenter, presence: true, length: {minimum:2, maximum:30}
+  validates :comment_text, presence: true, length: {minimum:2, maximum:300}
 end
