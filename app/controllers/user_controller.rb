19b4a0d7fb3bd42b7db6c6600f82622a02227867
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-22 11:16:57 +0800

    Renames users controller to user to access resource routes

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,38 @@
+class UserController < ApplicationController
+  before_action :find_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @blogs = @user.blogs
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_index_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # byebug
+    @user.destroy
+    sign_out_and_redirect(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :firstname, :lastname ,:password)
+  end
+
+  
+end
