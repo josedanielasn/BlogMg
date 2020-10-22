2b4b9df7121da503d46febb64ee7343b4970e043
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-22 16:23:37 +0800

    Adds testing for validation of blogs

---
+++ spec/models/blog_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Blog, type: :model do
+  let!(:blog) {create(:blog)}
+
+  context "when validate" do
+
+    context "when blog is valid" do
+      let(:valid_blog) {build(:blog)}
+      it {expect(valid_blog).to be_valid}
+    end
+    
+    context "when title is invalid" do
+      let(:blog1) {build(:blog, title: nil)}
+      let(:blog2) {build(:blog, title: '1')}
+      let(:blog3) {build(:blog, title: '1' * 51)}
+
+      it {expect(blog1).to_not be_valid}
+      it {expect(blog2).to_not be_valid}
+      it {expect(blog3).to_not be_valid}
+    end
+    
+    context "when title is invalid" do
+      let(:blog1) {build(:blog, content_text: nil)}
+      let(:blog2) {build(:blog, content_text: '1')}
+      let(:blog3) {build(:blog, content_text: '1' * 1001)}
+
+      it {expect(blog1).to_not be_valid}
+      it {expect(blog2).to_not be_valid}
+      it {expect(blog3).to_not be_valid}
+    end
+    
+  end
+  
+end
